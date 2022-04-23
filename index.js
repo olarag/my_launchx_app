fe6d5841b7a5a8ee3097bf14807bd93112ce0bd8
olarag <oscar@difusion.mx>
2022-04-23 13:18:43 -0500

    se agrega archivo index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+cont MissionCommander = require('./app/missionCommander');
+
+const MC1 = new MissionCommander("Carlo")
+console.log(MC1.name)
+
+const MC2 = new MissionCommander("Fernanda")
+console.log(MC2.name)
+
+const MC3 = new MissionCommander("Rodrigo")
+console.log(MC3.name)
